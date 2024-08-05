861023d62b1152a47a45d0ebc6eb5fd82124f0b0
Dinesh <dineshlegha23@gmail.com>
2024-08-05 13:24:41 +0530

    fixed deployment issues

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 require("express-async-errors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const rateLimiter = require("express-rate-limiter");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -16,14 +21,20 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan("tiny"));
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(express.static("./public"));
 app.use(cookieParser(process.env.JWT_SECRET));
-
-app.get("/", (req, res) => {
-  res.json({ msg: "success" });
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
